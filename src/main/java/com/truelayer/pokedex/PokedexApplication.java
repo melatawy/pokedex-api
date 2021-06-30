ef8e921fbfaf5ce026c3f8c17f30fb8bb818baeb
Maher El-Atawy <elatawy@amazon.com>
2021-06-30 17:26:02 +0100

    Add main application and its test

--- src/main/java/com/truelayer/pokedex/PokedexApplication.java
+++ src/main/java/com/truelayer/pokedex/PokedexApplication.java
@@ -1,13 +1,22 @@
 package com.truelayer.pokedex;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class PokedexApplication {
-
+    private static final Logger log = LoggerFactory.getLogger(PokedexApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(PokedexApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
 }
--- src/test/java/com/truelayer/pokedex/PokedexApplicationTests.java
+++ src/test/java/com/truelayer/pokedex/PokedexApplicationTests.java
@@ -1,13 +1,29 @@
 package com.truelayer.pokedex;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class PokedexApplicationTests {
 
+    @Autowired
+    private PokedexApplication application;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     @Test
     void contextLoads() {
+        assertThat(application).isNotNull();
+    }
+
+    @Test
+    void buildsRestTemplate() {
+        assertThat(restTemplate).isNotNull();
     }
 
 }
